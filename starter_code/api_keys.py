6a406482b2c8da6182d9b098f2a66c680c16b2bb
jkurtb <jkurt.baltero@gmail.com>
2020-06-18 03:31:08 -0400

    successfully retrieved API data, created df & csv files

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "e7521621e60d0ac1c9937959fcc798b0"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyCjhnWZVta2CitGSr8z5yLxYy8ue_oRXzU"
